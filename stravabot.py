71647ae63fdce4a91a36bdb38e57d1cc18d95e52
dznuzz <dnuzzomueller@gmail.com>
2016-12-29 18:03:41 -0500

    fixing some things.

--- stravabot.py
+++ stravabot.py
@@ -55,10 +55,10 @@ def filter_rides(rides):
     return filtered_rides
 
 def bl_regex_check(ridename):
-	for blregex in config["blacklistridenames"]:
-		if(re.search(blregex,ridename) is not None)
-			return True
-	return False
+    for blregex in config["blacklistridenames"]:
+        if(re.search(blregex,ridename) is not None):
+            return True
+    return False
     
 def slack_ride(ride):
     msg='<https://www.strava.com/activities/'+str(ride.id)+'|'+ride.athlete.firstname+' '+ride.athlete.lastname+': '+ride.name+'>';
@@ -69,25 +69,25 @@ def slack_ride(ride):
                 icon_emoji=config["slack_icon_emoji"],
                 unfurl_links=True,
                 unfurl_media=True)
-	if(ride.athlete.lastname is in config["HeckleName"]):
-		heckle(ride)
+    if(ride.athlete.lastname in config["heckle_names"]):
+        heckle(ride)
     return
 
-	
+    
 def heckle(ride):
     msg=random.choice(hecklestrings)
-	msg=msg.replace("$name",ride.firstname)
-	msg=msg.replace("$miles",str(ride.distance / 1609.34))
-	msg=msg.replace("$candy",str(ride.calories / 1500))
-	sc.api_call("chat.postMessage",
-				channel=config["slack_channel"],
-				text=msg,
-				username=config["slack_username"],
+    msg=msg.replace("$name",ride.firstname)
+    msg=msg.replace("$miles",str(ride.distance / 1609.34))
+    msg=msg.replace("$candy",str(ride.calories / 1500))
+    sc.api_call("chat.postMessage",
+                channel=config["slack_channel"],
+                text=msg,
+                username=config["slack_username"],
                 icon_emoji=config["slack_icon_emoji"],
                 unfurl_links=True,
                 unfurl_media=True)
-	return
-	
+    return
+    
 def is_new_ride(ride):
         # lookup ride in dynamo
         response = dynamodbTable.query(
